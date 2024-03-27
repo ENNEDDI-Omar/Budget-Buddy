e39cecbc7fe7a14aa9252ba3f600c497d08d9842
OmarENNEDDI <enneddiomar@gmail.com>
2024-03-27 10:56:03 +0000

    Add ExpenseFactory for generating expense data

--- database/migrations/2024_03_25_151838_create_expenses_table.php
+++ database/migrations/2024_03_25_151838_create_expenses_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('name')->unique();
             $table->string('amount')->nullable();
             $table->date('date')->nullable();
-            $table->string('type')->nullable();
             $table->softDeletes();
             $table->timestamps();
 
