d29d529c8710da7e634d46b1de5338f1d17b2966
OmarENNEDDI <enneddiomar@gmail.com>
2024-03-27 10:55:45 +0000

    Add User model to AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use App\Models\Expense;
+use App\Models\User;
 use App\Policies\ExpensePolicy;
 
 class AuthServiceProvider extends ServiceProvider
@@ -12,10 +13,11 @@ class AuthServiceProvider extends ServiceProvider
     /**
      * The policy mappings for the application.
      *
-     * @var array
+     * @var array                                
      */
     protected $policies = [
         Expense::class => ExpensePolicy::class,
+        
     ];
 
     /**
