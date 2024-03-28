164fecbb4f26eb4f23a55b73c170b874c86bca4d
OmarENNEDDI <enneddiomar@gmail.com>
2024-03-28 21:43:51 +0000

    Update ExpenseTest with new expense data

--- tests/Unit/ExpenseTest.php
+++ tests/Unit/ExpenseTest.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Artisan;
 
 class ExpenseTest extends TestCase
 {
-    use RefreshDatabase;
+    // use RefreshDatabase;
 
     public function test_index_returns_user_expenses()
     {
@@ -49,18 +49,18 @@ class ExpenseTest extends TestCase
         $response = $this->withHeaders([
             'Authorization' => 'Bearer ' . $token,
         ])->postJson('/api/expenses', [
-            'name' => 'New Expense',
-            'amount' => 100,
-            'date' => '2024-03-30', // Exemple de date
+            'name' => 'New Expense 4',
+            'amount' => 400,
+            'date' => '2024-03-20', // Exemple de date
             // Autres champs nécessaires pour créer une dépense
         ]);
 
         // Vérifier la réponse et l'état de la base de données après l'action de création
-        $response->assertStatus(201); // Le code de statut 201 indique que la création a réussi
+        $response->assertStatus(201); 
         $response->assertJsonFragment([
-            'name' => 'New Expense',
-            'amount' => 100,
-            'date' => '2024-03-30',
+            'name' => 'New Expense 4',
+            'amount' => 400,
+            'date' => '2024-03-20',
             'user_id' => $user->id,
         ]); // Vérifie si les données créées correspondent aux données envoyées
 
@@ -79,17 +79,19 @@ class ExpenseTest extends TestCase
         $response = $this->withHeaders([
             'Authorization' => 'Bearer ' . $token,
         ])->putJson('/api/expenses/' . $expense->id, [
-            'name' => 'Updated Expense',
-            'amount' => 200,
-            'date' => '2024-03-31', // Exemple de date
+            'name' => 'Updated Expense 555',
+            'amount' => 222,
+            'date' => '2024-03-31',
+            'user_id' => $user->id,
             // Autres champs nécessaires pour mettre à jour une dépense
         ]);
 
         // Vérifier la réponse et l'état de la base de données après l'action de mise à jour
         $response->assertStatus(200); // Le code de statut 200 indique que la mise à jour a réussi
-        $response->assertJsonFragment([
-            'name' => 'Updated Expense',
-            'amount' => 200,
+        $this->assertDatabaseHas('expenses', [
+            'id' => $expense->id,
+            'name' => 'Updated Expense 555',
+            'amount' => 222,
             'date' => '2024-03-31',
             'user_id' => $user->id,
         ]); // Vérifie si les données mises à jour correspondent aux données envoyées
